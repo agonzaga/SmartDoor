7e382a67a337c797e97c1206d4cdb828f49fc4f6
Andre <agonzaga@bu.edu>
2018-12-11 05:40:37 +0000

    updates

--- control.py
+++ control.py
@@ -1,12 +1,14 @@
 import face_recog
 import os, sys
+import sender
 
-def main(name):
-	arg = "python3 face_recog.py -n 2 -a " + name
+def main(name, email):
+	arg = "python3 face_recog.py -n 5 -a " + name
 	os.system(arg)
+	sender.initialize(name)
 	
 	
 	
 	
 if __name__ == '__main__':
-	main(sys.argv[1])
+	main(sys.argv[1], sys.argv[2])
