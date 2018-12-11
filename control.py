5b2af49ba4642d2a9949749b143a08b6f7b53eef
Andre <agonzaga@bu.edu>
2018-12-11 06:34:30 +0000

    updates

--- control.py
+++ control.py
@@ -1,13 +1,14 @@
 import face_recog
 import os, sys
 import sender
+import email_sender
 
 def main(name, email):
-	arg = "python3 face_recog.py -n 5 -a " + name
-	os.system(arg)
+	os.system("sudo modprobe bcm2835-v4l2")
+	arg = "python3 face_recog.py -n 2 -a " + name
+	#os.system(arg)
 	sender.initialize(name)
-	
-	
+	email_sender.send_email(email)
 	
 	
 if __name__ == '__main__':
